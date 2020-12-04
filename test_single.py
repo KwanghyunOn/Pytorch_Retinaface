a591ca9ec2518b799adfda34e62ce4125daad31e
KwanghyunOn <kwanghyun.on@gmail.com>
2020-12-04 08:36:07 +0000

    Toy test end

--- test_single.py
+++ test_single.py
@@ -12,19 +12,20 @@ from models.retinaface import RetinaFace
 from utils.box_utils import decode, decode_landm
 from utils.timer import Timer
 
-parser = argparse.ArgumentParser(description='Retinaface')
 
+parser = argparse.ArgumentParser(description='Retinaface')
 parser.add_argument('-m', '--trained_model', default='./weights/mobilenet0.25_Final.pth',
                     type=str, help='Trained state_dict file path to open')
 parser.add_argument('--network', default='mobile0.25', help='Backbone network mobile0.25 or resnet50')
-parser.add_argument('--save_folder', default='eval/', type=str, help='Dir to save results')
+parser.add_argument('--origin_size', default=False, type=str, help='Whether use origin image size to evaluate')
+parser.add_argument('--save_folder', default='./widerface_evaluate/widerface_txt/', type=str, help='Dir to save txt results')
 parser.add_argument('--cpu', action="store_true", default=False, help='Use cpu inference')
-parser.add_argument('--dataset', default='FDDB', type=str, choices=['FDDB'], help='dataset')
+parser.add_argument('--dataset_folder', default='./data/widerface/val/images/', type=str, help='dataset path')
 parser.add_argument('--confidence_threshold', default=0.02, type=float, help='confidence_threshold')
 parser.add_argument('--top_k', default=5000, type=int, help='top_k')
 parser.add_argument('--nms_threshold', default=0.4, type=float, help='nms_threshold')
 parser.add_argument('--keep_top_k', default=750, type=int, help='keep_top_k')
-parser.add_argument('-s', '--save_image', action="store_true", default=False, help='show detection results')
+parser.add_argument('-s', '--save_image', action="store_true", default=True, help='show detection results')
 parser.add_argument('--vis_thres', default=0.5, type=float, help='visualization_threshold')
 args = parser.parse_args()
 
@@ -67,10 +68,15 @@ def load_model(model, pretrained_path, load_to_cpu):
 
 if __name__ == '__main__':
     torch.set_grad_enabled(False)
-    cfg = cfg_mnet
+
+    cfg = None
+    if args.network == "mobile0.25":
+        cfg = cfg_mnet
+    elif args.network == "resnet50":
+        cfg = cfg_re50
+    # net and model
     net = RetinaFace(cfg=cfg, phase = 'test')
-    pretrained_path = './weights/mobilenet0.25_Final.pth'
-    net = load_model(net, pretrained_path, args.cpu)
+    net = load_model(net, args.trained_model, args.cpu)
     net.eval()
     print('Finished loading model!')
     print(net)
@@ -78,21 +84,40 @@ if __name__ == '__main__':
     device = torch.device("cpu" if args.cpu else "cuda")
     net = net.to(device)
 
-    # save file
-    if not os.path.exists(args.save_folder):
-        os.makedirs(args.save_folder)
-    fw = open(os.path.join(args.save_folder, args.dataset + '_dets.txt'), 'w')
+    # testing dataset
+    testset_folder = args.dataset_folder
+    testset_list = args.dataset_folder[:-7] + "wider_val.txt"
+
+    #with open(testset_list, 'r') as fr:
+    #    test_dataset = fr.read().split()
+    #num_images = len(test_dataset)
+
+    _t = {'forward_pass': Timer(), 'misc': Timer()}
 
     if not os.path.exists("./results/"):
         os.makedirs("./results/")
-    result_path = "./results/single_person_result.jpg"
+    result_path = "./results/single_person_result2.jpeg"
+
+    # eval start
     image_path = "./test_images/single_person.jpeg"
-
-    resize = 0.5
-    _t = {'forward_pass': Timer(), 'misc': Timer()}
-
     img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
     img = np.float32(img_raw)
+
+    # testing scale
+    target_size = 1600
+    max_size = 2150
+    im_shape = img.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+    resize = float(target_size) / float(im_size_min)
+    # prevent bigger axis from being more than max_size:
+    if np.round(resize * im_size_max) > max_size:
+        resize = float(max_size) / float(im_size_max)
+    if args.origin_size:
+        resize = 1
+
+    if resize != 1:
+        img = cv2.resize(img, None, None, fx=resize, fy=resize, interpolation=cv2.INTER_LINEAR)
     im_height, im_width, _ = img.shape
     scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
     img -= (104, 117, 123)
@@ -128,8 +153,8 @@ if __name__ == '__main__':
     scores = scores[inds]
 
     # keep top-K before NMS
-    # order = scores.argsort()[::-1][:args.top_k]
     order = scores.argsort()[::-1]
+    # order = scores.argsort()[::-1][:args.top_k]
     boxes = boxes[order]
     landms = landms[order]
     scores = scores[order]
@@ -137,7 +162,7 @@ if __name__ == '__main__':
     # do NMS
     dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
     keep = py_cpu_nms(dets, args.nms_threshold)
-
+    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
     dets = dets[keep, :]
     landms = landms[keep]
 
@@ -148,7 +173,31 @@ if __name__ == '__main__':
     dets = np.concatenate((dets, landms), axis=1)
     _t['misc'].toc()
 
-    # show image
+    """
+    # --------------------------------------------------------------------
+    save_name = args.save_folder + img_name[:-4] + ".txt"
+    dirname = os.path.dirname(save_name)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    with open(save_name, "w") as fd:
+        bboxs = dets
+        file_name = os.path.basename(save_name)[:-4] + "\n"
+        bboxs_num = str(len(bboxs)) + "\n"
+        fd.write(file_name)
+        fd.write(bboxs_num)
+        for box in bboxs:
+            x = int(box[0])
+            y = int(box[1])
+            w = int(box[2]) - int(box[0])
+            h = int(box[3]) - int(box[1])
+            confidence = str(box[4])
+            line = str(x) + " " + str(y) + " " + str(w) + " " + str(h) + " " + confidence + " \n"
+            fd.write(line)
+
+    print('im_detect: {:d}/{:d} forward_pass_time: {:.4f}s misc: {:.4f}s'.format(i + 1, num_images, _t['forward_pass'].average_time, _t['misc'].average_time))
+    """
+
+    # save image
     if args.save_image:
         for b in dets:
             if b[4] < args.vis_thres:
@@ -169,3 +218,4 @@ if __name__ == '__main__':
             cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
         # save image
         cv2.imwrite(result_path, img_raw)
+
